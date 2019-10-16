7df28a17594cf67b4805ac39153654e0d81050ab
Krystian Bieniek <krystekk2013@gmail.com>
2019-10-16 17:31:25 +0200

    Updates

---
+++ precache-manifest.a87454ea7468f6a1352613bf73e1d83c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd6f99c4a7a1a02baec7",
+    "url": "/AJAX-API-JSONPlaceholder/static/js/runtime~main.cd6f99c4.js"
+  },
+  {
+    "revision": "d28ff51517cd50a17457",
+    "url": "/AJAX-API-JSONPlaceholder/static/js/main.d28ff515.chunk.js"
+  },
+  {
+    "revision": "2eb1efbf424eb56e824f",
+    "url": "/AJAX-API-JSONPlaceholder/static/js/1.2eb1efbf.chunk.js"
+  },
+  {
+    "revision": "d28ff51517cd50a17457",
+    "url": "/AJAX-API-JSONPlaceholder/static/css/main.1fc6a18f.chunk.css"
+  },
+  {
+    "revision": "6a00b7a1464eee42d8386415eaa150b4",
+    "url": "/AJAX-API-JSONPlaceholder/index.html"
+  }
+];

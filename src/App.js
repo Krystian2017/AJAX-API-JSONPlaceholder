9d944cb02f9bfe5d15e2765f316bf853231ab841
Krystian Bieniek <krystekk2013@gmail.com>
2019-10-16 17:24:47 +0200

    AJAX from JSONPlaceholder

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     users: []
+  //   }
+  //   this.requestData()
+  // }
+
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+    // this.requestData()
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+
+    // xhr.onload = () => {
+    //   console.log(xhr.status);
+    //   if (xhr.status === 200) {
+    //     const users = JSON.parse(xhr.response)
+    //     console.log(users);
+    //     this.setState({ users })
+    //   }
+    //   // console.log(typeof xhr.response);
+    // }
+
+    xhr.addEventListener("load", () => {
+      console.log(xhr.status);
+      if (xhr.status === 200) {
+        const users = JSON.parse(xhr.response);
+        console.log(users);
+        this.setState({ users });
+      }
+    });
+
+    xhr.send(null);
+  }
+
+  // requestData() {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+
+  //   xhr.onload = () => {
+  //     console.log(xhr.status);
+  //     if (xhr.status === 200) {
+  //       const users = JSON.parse(xhr.response)
+  //       console.log(users);
+  //       this.setState({ users })
+  //     }
+  //     // console.log(typeof xhr.response);
+  //   }
+  //   xhr.send(null)
+  // }
+
+  render() {
+    const users = this.state.users.map(user => (
+      <div key={user.id}>
+        <h4>{user.name}</h4>
+        <p>{user.address.city}</p>
+      </div>
+    ));
+    return (
+      <div className="App">
+        <div>
+          <h1 className="App-header">AJAX REST API</h1>
+          <p className="App-description">
+            Task to practise REST API and XMLHttpRequest - I used create-app and
+            AJAX to load file from{" "}
+            <a className="App-link" href="https://jsonplaceholder.typicode.com">
+              JSONPlaceholder
+            </a>
+            . It is simply app and you can find the code on{" "}
+            <a
+              className="App-link"
+              href="https://github.com/Krystian2017/AJAX-REST-API-JSONPlaceholder"
+            >
+              my GitHub account.
+            </a>
+          </p>
+        </div>
+        <div>{users}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
